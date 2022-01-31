bd8ef4312875da3706495737bd17fff18502f7b7
Thales Faggiano <thalesfaggiano@gmail.com>
2022-01-31 18:17:44 -0300

    First number

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,20 +6,29 @@
 /*   By: tfaggian <tfaggian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 07:48:06 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/01/31 08:59:31 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/01/31 18:11:03 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void ft_print_combn(int n)
+void	ft_print_combn(int n)
 {
 	int	count[n];
+	int	max;
 	int	i;
+	char	c;
 
+	i = n;
+	max = n;
 	if (n > 9 && n < 1)
 		return ;
-	i = n + 1;
 	while(i--)
 	{
 		count[i] = i;
+	}
+	while(++i < max)
+	{
+		c = count[i] + 0x30;
+		write(1, &c, 1);
 	}
 }
