12afe1d8896d3bf2ab96f7b4b4b1a66d2ab1d730
Thales Faggiano <thalesfaggiano@gmail.com>
2022-01-31 21:28:29 -0300

    New Tests

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,29 +6,36 @@
 /*   By: tfaggian <tfaggian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 07:48:06 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/01/31 18:11:03 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/01/31 20:18:02 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
+static char	g_c[9];
+
+void	ft_rec(int size, int i, char dig);
+
 void	ft_print_combn(int n)
 {
-	int	count[n];
-	int	max;
-	int	i;
-	char	c;
+	if (n < 10 && n > 0)
+		ft_rec(n, 0, 0x30);
+}
 
-	i = n;
-	max = n;
-	if (n > 9 && n < 1)
-		return ;
-	while(i--)
+void	ft_rec(int size, int i, char dig)
+{
+	if (i == size)
 	{
-		count[i] = i;
+		write(1, g_c, size);
+		if (g_c[0] < 0x3a - size)
+			write(1, ", ", 2);
 	}
-	while(++i < max)
+	else
 	{
-		c = count[i] + 0x30;
-		write(1, &c, 1);
+		while (dig < 0x3a)
+		{
+			g_c[i] = dig;
+			ft_rec(size, i + 1, dig + 1);
+			++dig;
+		}
 	}
 }
