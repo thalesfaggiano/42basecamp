97962708c4373f4a7e58740538d4cbc9a7c5fe77
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-02 07:31:17 -0300

    mainc02_ex00.c

--- c02/ex00/mainc02_ex00.c
+++ c02/ex00/mainc02_ex00.c
@@ -6,10 +6,11 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 22:44:12 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/01 23:14:53 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/02 07:27:44 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strcpy(char *dest, char *src);
 
@@ -18,6 +19,7 @@ int	main(void)
 	char	src[10] = "Ola Mundo";
 	char	dest[10];
 
+	printf("%s\n", strcpy(dest, src));
 	printf("%s\n", ft_strcpy(dest, src));
 	return (0);
 }
