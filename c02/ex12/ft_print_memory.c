4b4dd4b1ad01fe2aacde4ef97051f72033a976fc
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-03 13:40:45 -0300

    Improuvements

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 21:48:09 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/03 11:17:00 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/03 13:39:59 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -18,11 +18,12 @@ void	*ft_print_memory(void *addr, unsigned int size)
 {
 	unsigned int	i;
 
+	if (size == 0)
+		return (addr);
 	i = 0;
 	while (i * 0x10 <= size)
 	{
 		col_addr((size_t)addr + (i * 0x10));
-		write(1, "\x3a\x20", 2);
 		col_str(addr + (i * 0x10));
 		write(1, "\x0a", 1);
 		++i;
@@ -37,13 +38,14 @@ void	col_addr(size_t addr)
 	int	i;
 	
 	dig = "0123456789abcdef";
-	i = 18;
-	while (--i > 0)
+	i = 16;
+	while (--i > -1)
 	{
 		hex[i] = dig[addr % 0x10];
 		addr /= 0x10;
 	}
 	write(1, hex, 16);
+	write(1, "\x3a\x20", 2);
 }
 
 
--- c02/ex12/mainc02_ex12.c
+++ c02/ex12/mainc02_ex12.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 21:35:43 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/03 11:51:34 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/03 13:16:10 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(void)
 	unsigned int	size;
 	char		*var;
 
-	var = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-	size = 0x31;
+	var = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBB";
+	size = 0x14;
 	printf("%p - %d - %s\n",var, size, var);
 	printf("%p\n",ft_print_memory(var, size));
 	return (0);
