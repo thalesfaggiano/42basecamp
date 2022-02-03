7ef16eb364dc9cc15b2dc53b16ae51d16a1d09b9
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-03 09:59:09 -0300

    First Column finished

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,26 +6,25 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 21:48:09 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/03 08:53:33 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/03 09:41:51 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-void	mem_addr(size_t addr)
+void	col_addr(size_t addr)
 {
 	char	*dig;
 	char	hex[16];
 	int	i;
 	
-	dig = "0123456789abcdef:";
+	dig = "0123456789abcdef";
 	i = 16;
-	while (addr)
+	while (i-- > -1)
 	{
-		hex[--i] = dig[addr % 0x10];
+		hex[i] = dig[addr % 0x10];
 		addr /= 0x10;
 	}
 	write(1, hex, 16);
-	write(1, &dig[16], 1);
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
@@ -33,12 +32,13 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	unsigned int	i;
 	char		*eol;
 
-	eol = "\n";
+	eol = ":\n";
 	i = 0;
 	while (i * 0x10 <= size)
 	{
-		mem_addr((size_t)addr + (i * 0x10));
-		write(1, eol, 1);
+		col_addr((size_t)addr + (i * 0x10));
+		write(1, &eol[0], 1);
+		write(1, &eol[1], 1);
 		++i;
 	}
 	return (addr);
