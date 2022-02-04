618fa5982bc4753847d673eb6d8156a48faa83d9
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-04 09:42:48 -0300

    ex00 test improuvement

--- c03/ex00/mainc03_ex00.c
+++ c03/ex00/mainc03_ex00.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:47:22 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/04 09:27:33 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/04 09:42:25 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ int	ft_strcmp(char *s1, char *s2);
 
 int	main(void)
 {
-	char	*s1 = "ii";
-	char	*s2 = "iii";
-	char	*s3 = "ii";
-	char	*s4 = "iii";
+	char	*s1 = "i";
+	char	*s2 = "ii";
 	int	a;
 	int	b;
 
 	a = strcmp(s1, s2);
 	printf(" a - %d\ns1 - %s\ns2 - %s\n", a, s1, s2);
-	b = ft_strcmp(s3, s4);
+	b = ft_strcmp(s1, s2);
 	printf(" b - %d\ns3 - %s\ns4 - %s\n", b, s3, s4);
 	return (0);
 }
