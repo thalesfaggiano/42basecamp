1c07f6d332d2598f28d6fac5d9b8c803b9f1b979
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-04 18:05:21 -0300

    Starting ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,8 +6,23 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 13:57:08 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/04 13:57:13 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/04 17:59:12 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strstr(char *str, char *to_find);
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	k;
+
+	i = -1;
+	while (str[++i])
+	{
+		k = 0;
+		while (str[i + k] == to_find[k])
+			++k;
+		if (!(to_find[k]))
+			return (&str[i]);
+	}
+	return (0);
+}
--- c03/ex04/mainc03_ex04.c
+++ c03/ex04/mainc03_ex04.c
@@ -6,24 +6,24 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 13:55:58 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/04 14:12:05 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/04 18:00:15 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <string.h>
 
-//char	*ft_strstr(char *str, char *to_find);
+char	*ft_strstr(char *str, char *to_find);
 
 int	main(void)
 {
 	char	*str = "Eu te amo meu amor lindo!";
-	char	*to_find = "amor";
-	char	strstr[100];
-	//char	strstr2[100];
+	char	*to_find = " ";
+	char	*strstr1;
+	char	*strstr2;
 
-	strstr = strstr(str, to_find);
-	printf("%s\n", strstr);
-	//strstr2 = ft_strstr(str, to_find);
-	//printf("%s\n", strstr2);
+	strstr1 = strstr(str, to_find);
+	printf("1 - %s\n", strstr1);
+	strstr2 = ft_strstr(str, to_find);
+	printf("2 - %s\n", strstr2);
 	return (0);
 }
