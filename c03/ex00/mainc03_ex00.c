b155f132996cb98efda96c8744cca192970d265b
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-04 09:45:07 -0300

    fixing main

--- c03/ex00/mainc03_ex00.c
+++ c03/ex00/mainc03_ex00.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:47:22 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/04 09:42:25 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/04 09:44:42 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ int	main(void)
 	a = strcmp(s1, s2);
 	printf(" a - %d\ns1 - %s\ns2 - %s\n", a, s1, s2);
 	b = ft_strcmp(s1, s2);
-	printf(" b - %d\ns3 - %s\ns4 - %s\n", b, s3, s4);
+	printf(" b - %d\ns3 - %s\ns4 - %s\n", b, s1, s2);
 	return (0);
 }
