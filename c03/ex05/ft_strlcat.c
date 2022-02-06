ec72a64bc9a7f3e7f6e220f4297465887b45e628
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-06 14:25:55 -0300

    Finished_final ex05 kkk

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:49:30 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/06 10:42:51 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/06 14:20:15 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	s;
 
 	d = 0;
-	s = d - 1;
+	s = --d;
 	while(dest[++d]);
-	while(d + ++s < size && src[s])
+	while(d + ++s < size - 1 && src[s])
 		dest[d + s] = src[s];
-	dest[d + --s] = 0x00;
+	dest[d + s--] = 0x00;
 	while(src[++s]);
 	return (d < size ? d + s : size + s);
 }
--- c03/ex05/mainc03_ex05.c
+++ c03/ex05/mainc03_ex05.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:47:22 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/06 09:51:12 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/06 14:25:11 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -16,16 +16,16 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 
 int	main(void)
 {
-	char	dest[20] = "Tha";
-	char	src[20] = "lesTha";
-	char	dest1[20] = "Tha";
-	char	src1[20] = "lesTha";
+	char	dest[20] = "123";
+	char	src[20] = "Thal";
+	char	dest1[20] = "123";
+	char	src1[20] = "Thal";
 	unsigned int	a;
 	unsigned int	b;
 
 	a = strlcat(dest, src, (unsigned) 7);
-	printf("dest- %s\nsrc - %s\nsize = %d\n", dest, src, a);
+	printf("dest- %s\nsrc- %s\nsize = %d\n", dest, src, a);
 	b = ft_strlcat(dest1, src1, (unsigned) 7);
-	printf("dest -%s\nsrc - %s\nsize = %d\n", dest1, src1, b);
+	printf("dest- %s\nsrc- %s\nsize = %d\n", dest1, src1, b);
 	return (0);
 }
