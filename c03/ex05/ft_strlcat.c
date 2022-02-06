19941cecf291c2812a1908847c23c821da0e3097
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-06 14:45:45 -0300

    Fixing some syntax

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:49:30 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/06 14:20:15 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/06 14:39:32 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 
 	d = 0;
 	s = --d;
-	while(dest[++d]);
-	while(d + ++s < size - 1 && src[s])
+	while (dest[++d]);
+	while (d + ++s < size - 1 && src[s])
 		dest[d + s] = src[s];
 	dest[d + s--] = 0x00;
-	while(src[++s]);
+	while (src[++s]);
 	return (d < size ? d + s : size + s);
 }
