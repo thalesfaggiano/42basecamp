a4561304de354691fbf4b5f142938dc556259234
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-11 10:16:44 -0300

    Put Positive Numbers

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,8 +6,54 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 08:21:16 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/10 08:21:30 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/11 10:16:06 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void ft_putnbr_base(int nbr, char *base);
+void	ft_nbr(int nbr, char *base, int size);
+int	*ft_validation(char *base);
+int	g_size;
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	if (*ft_validation(base))
+		ft_nbr(nbr, base, *ft_validation(base));
+}
+
+void	ft_nbr(int nbr, char *base, int size)
+{
+	char	number[100];
+	int	i;
+	
+	i = -1;
+	while (nbr)
+	{
+		number[++i] = base[nbr % size];
+		nbr /= size;
+	}
+	while (i + 1)
+		write(1, &number[i--] ,1);
+}
+
+int	*ft_validation(char *base)
+{
+	int	i;
+	int	k;
+
+	i = -1;
+	while (base[++i])
+	{
+		k = -1;
+		if (base[i] == 0x2b || base[i] == 0x2d
+			|| base[i] < 0x20 || base[i] > 0x7e)
+			return (0);
+		while (++k < i)
+			if (base[k] == base[i])
+				return (0);
+	}
+	if (i < 2)
+		return (0);
+	g_size = i;
+	return (&g_size);
+}
--- c04/ex04/mainc04_ex04.c
+++ c04/ex04/mainc04_ex04.c
@@ -6,8 +6,14 @@
 /*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 08:20:20 by tfaggian          #+#    #+#             */
-/*   Updated: 2022/02/10 08:20:26 by tfaggian         ###   ########.fr       */
+/*   Updated: 2022/02/11 10:14:27 by tfaggian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putnbr_base(int nbr, char *base);
+void	ft_putnbr_base(int nbr, char *base);
+
+int	main(void)
+{
+	ft_putnbr_base(16, "0123456789abcdef");
+	return (0);
+}
