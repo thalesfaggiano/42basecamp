e798418bd384ba269a7b0b7167323da0e181bb44
Thales Faggiano <thalesfaggiano@gmail.com>
2022-02-12 17:59:16 -0300

    Starting ex05

---
+++ c04/ex05/mainc04_ex05.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainc04_ex05.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfaggian <tfaggian@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 19:16:28 by tfaggian          #+#    #+#             */
+/*   Updated: 2022/02/11 19:18:26 by tfaggian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_atoi_base(char *str, char *base);
+
+int	main(void)
+{
+	char	*str;
+	char	*base;
+
+	ft_atoi_base(str, base);
+	return (0);
+}
